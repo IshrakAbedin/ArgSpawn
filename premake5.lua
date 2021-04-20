b97cf4268dac05ce96b51096d016c5d5ca9ee999
Ishrak Abedin <ishrak.abedin.a1@gmail.com>
2021-04-20 23:46:04 +0600

    Initial Commit

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+-- premake5.lua
+workspace "ArgSpawn"
+   configurations { "Debug", "Release" }
+   
+   project "ArgSpawn"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+   architecture "x64"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   includedirs {
+        "./src",
+        "./src/vendor/fmt/include",
+        "./src/vendor/yaml-cpp/include"
+    }
+
+   files { "**.h", "**.hpp", "**.cpp", "**.cc", "**.cx", "**.c" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
---
+++ src/Application.cpp
@@ -0,0 +1,7 @@
+#include "fmt/core.h"
+
+int main()
+{
+	fmt::print("Hello World {0}.", 51);
+	return 0;
+}
